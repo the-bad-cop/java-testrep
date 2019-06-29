e43288a912d9a4a5dff32d5c9203386210a4dd76
fear <abc@example.com>
2019-06-29 10:10:40 +0530

    Python commit

---
+++ test.py
@@ -0,0 +1,39 @@
+import math
+print("Hello WOrld")
+#x = input("Enter")
+# print(x)
+"""
+
+def myMeth():
+    print("Hello WOrld\n")
+
+
+def newMeth():
+    a = input("ENter the number")
+
+
+def new():
+    print("Hello WORLD")
+
+
+print("Hello")
+
+"""
+
+
+class myClass:
+    name = ""
+    age = 20
+
+    def method(self):
+        print("Inside class")
+
+    def method2(self):
+        print(self.name, self.age)
+
+
+obj = myClass()
+obj.name = "Sam"
+obj.age = 10
+obj.method()
+obj.method2()
